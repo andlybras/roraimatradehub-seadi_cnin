25a8139313abfad5def5bda9cd25e5a9c30e0308
Andly <andlybras.desenvolvedor@outlook.com>
2025-09-02 13:59:01 -0400

    Refatora o painel de admin para possibilitar a visualização dos perfis empresariais separados

--- accounts_management/admin.py
+++ accounts_management/admin.py
@@ -2,8 +2,10 @@
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
+# Importamos o modelo original e os novos modelos proxy
+from .models import CustomUser, EmpresaUser, EmpreendedorUser
 
+# Mantemos a classe de admin base que já tínhamos
 class CustomUserAdmin(UserAdmin):
     add_fieldsets = UserAdmin.add_fieldsets + (
         (None, {'fields': ('user_type',)}),
@@ -12,5 +14,26 @@ class CustomUserAdmin(UserAdmin):
         (None, {'fields': ('user_type',)}),
     )
     list_display = ('username', 'email', 'user_type', 'is_staff')
+    # Adicionamos um filtro na lateral para facilitar a busca
+    list_filter = ('user_type', 'is_staff', 'is_superuser', 'groups')
 
-admin.site.register(CustomUser, CustomUserAdmin)
+
+# --- NOVAS CLASSES E REGISTROS ---
+
+@admin.register(EmpresaUser)
+class EmpresaUserAdmin(CustomUserAdmin):
+    """
+    Classe de Admin específica para exibir apenas usuários do tipo 'Empresa'.
+    """
+    def get_queryset(self, request):
+        # Sobrescrevemos o método get_queryset para filtrar os resultados
+        return super().get_queryset(request).filter(user_type='empresa')
+
+
+@admin.register(EmpreendedorUser)
+class EmpreendedorUserAdmin(CustomUserAdmin):
+    """
+    Classe de Admin específica para exibir apenas usuários do tipo 'Empreendedor'.
+    """
+    def get_queryset(self, request):
+        return super().get_queryset(request).filter(user_type='empreendedor')
--- accounts_management/models.py
+++ accounts_management/models.py
@@ -12,3 +12,24 @@ class CustomUser(AbstractUser):
         default='empreendedor',
         verbose_name="Este perfil é para"
     )
+    
+class EmpresaUser(CustomUser):
+    """
+    Modelo Proxy para filtrar e exibir apenas usuários do tipo 'Empresa'.
+    """
+    class Meta:
+        # Define este modelo como um proxy. Nenhuma nova tabela será criada.
+        proxy = True
+        # Nomes que aparecerão no painel de administração
+        verbose_name = 'Perfil Empresarial - Empresa'
+        verbose_name_plural = 'Perfis Empresariais - Empresa'
+
+
+class EmpreendedorUser(CustomUser):
+    """
+    Modelo Proxy para filtrar e exibir apenas usuários do tipo 'Empreendedor'.
+    """
+    class Meta:
+        proxy = True
+        verbose_name = 'Perfil Empresarial - Empreendedor'
+        verbose_name_plural = 'Perfis Empresariais - Empreendedor'
